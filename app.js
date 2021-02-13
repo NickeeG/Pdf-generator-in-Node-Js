f0e546ac6e4f4b205fede07871476762c40001ec
unknown <ochodon@gmail.com>
2021-02-13 11:24:52 +0300

    updates made

--- app.js
+++ app.js
@@ -3,6 +3,31 @@ const fs = require('fs');
 const express=require('express');
 
 let pdfDoc = new PDFDocument;
-pdfDoc.pipe(fs.createWriteStream('SampleDocument.pdf'));
+generateHeader(pdfDoc);
+generateFooter(pdfDoc);
+pdfDoc.pipe(fs.createWriteStream('Samplepdf.pdf'));
 pdfDoc.text("My Sample PDF Document");
 pdfDoc.end();
+
+function generateHeader(pdfDoc) {
+    pdfDoc
+      .image("logo.jpg", 50, 45, { width: 50 })
+      .fillColor("#444444")
+      .fontSize(20)
+      .text("Lion Inc.", 110, 57)
+      .fontSize(10)
+      .text("123 Koinange street", 200, 65, { align: "right" })
+      .text("Nairobi, 10025", 200, 80, { align: "right" })
+      .moveDown();
+  }
+
+  function generateFooter(pdfDoc) {
+    pdfDoc
+      .fontSize(10)
+      .text(
+        "Great minds think alike",
+        50,
+        780,
+        { align: "center", width: 500 }
+      );
+  }
